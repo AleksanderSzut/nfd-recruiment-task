78c86da27c3dab7f27fe3d8b3d3214501ffece3b
szut <szut.aleksander@gmail.com>
2022-08-24 00:19:31 +0200

    feat: add company schema migration

--- database/migrations/2022_08_23_213738_create_companies_table.php
+++ database/migrations/2022_08_23_213738_create_companies_table.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,10 +13,17 @@ return new class extends Migration
      *
      * @return void
      */
-    public function up()
+    public function up(): void
     {
-        Schema::create('companies', function (Blueprint $table) {
+        Schema::create('companies', static function (Blueprint $table) {
             $table->id();
+
+            $table->integer('nip')->unique();
+            $table->string('name');
+            $table->tinyText('address');
+            $table->tinyText('city');
+            $table->tinyText('zip');
+
             $table->timestamps();
         });
     }
@@ -24,7 +33,7 @@ return new class extends Migration
      *
      * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('companies');
     }
